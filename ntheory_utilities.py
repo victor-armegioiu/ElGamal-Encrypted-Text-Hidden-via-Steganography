20d4c0ea4bc3287bbd060c96d22fc687c51490e2
Victor Armegioiu <victor.armegioiu@gmail.com>
2018-01-02 23:04:12 +0200

    Number theory utilities and primality tests

---
+++ ntheory_utilities.py
@@ -0,0 +1,162 @@
+from math import sqrt
+import random
+
+"""
+    @param: base 
+        base to be used for modular exponentiation
+
+    @param: exp
+        power to raise the base to
+
+    @param: modulo
+        value of the field we're working in, (Z/pZ)*
+
+    @returns: 
+        Computes (base ^ exp) % modulo in log2(exp) time.
+"""
+def mod_exp(base, exp, modulo):
+    ans = 1
+    while exp:
+        if exp & 1:
+            ans = (ans * base) % modulo
+
+        base = (base * base) % modulo
+        exp >>= 1
+
+    return ans
+
+"""
+    Computation of the Jacobi symbol (a / n) using modular congruences.
+    Calculated using the properties listed here :
+    https://en.wikipedia.org/wiki/Jacobi_symbol#Calculating_the_Jacobi_symbol   
+
+    @param: a
+        numerator of the jacobi symbol
+
+    @param: b
+        denominator of the jacobi symbol
+
+    @returns:
+        value of the jacobi symbol (a / n)
+"""
+def jacobi_symbol(a, n):
+    if n == 1:
+        return 1
+
+    elif a == 0:
+        return 0
+
+    elif a == 1:
+        return 1
+
+    elif a == 2:
+        if n % 8 in [3, 5]:
+            return -1
+        elif n % 8 in [1, 7]:
+            return 1
+
+    elif a < 0:
+        return (-1) ** ((n - 1) / 2) * jacobi_symbol(-1 * a, n)
+
+    if a % 2 == 0:
+        return jacobi_symbol(2, n) * jacobi_symbol(a / 2, n)
+
+    elif a % n != a:
+        return jacobi_symbol(a % n, n)
+
+    else:
+        if a % 4 == n % 4 == 3:
+            return -1 * jacobi_symbol(n, a)
+        else:
+            return jacobi_symbol(n, a)
+
+
+"""
+    @param: p
+        p is a prime s.t. p = 2 * q + 1 and q == prime
+        As such, the only prime divisors of p - 1 are
+        2 and (p - 1) / 2 = q;
+
+    @returns:
+        primitive root of the prime p iff p = 2 * q + 1
+"""
+def primitive_root(p):
+    s = p - 1
+    divisors = (2, s // 2)
+
+    while True:
+        g = random.randint(2, p - 1)
+        if mod_exp(g, s // divisors[0], p) != 1 and mod_exp(g, s // divisors[1], p) != 1:
+            return g
+
+"""
+    Quick computation of the U_{n + 1} and V_{n + 1} without
+    calculating intermediate terms.
+
+    https://en.wikipedia.org/wiki/Lucas_pseudoprime#Implementing_a_Lucas_probable_prime_test
+"""
+def U_V_subscript(k, n, U, V, P, Q, D):
+    k, n, U, V, P, Q, D = map(int, (k, n, U, V, P, Q, D))
+    digits = list(map(int, str(bin(k))[2 : ]))
+    subscript = 1
+
+    for digit in digits[1 : ]:
+        U, V = U * V  % n, (pow(V, 2, n) - 2 * pow(Q, subscript, n)) % n
+        subscript *= 2
+
+        if digit == 1:
+            if not (P * U + V) & 1:
+                if not (D * U + P * V) & 1:
+                    U, V = (P * U + V) >> 1, (D * U + P * V) >> 1
+
+                else:
+                    U, V = (P * U + V) >> 1, (D * U + P * V + n) >> 1
+
+            elif not (D * U + P * V) & 1:
+                U, V = (P * U + V + n) >> 1, (D * U + P * V) >> 1
+
+            else:
+                U, V = (P * U + V + n) >> 1, (D * U + P * V + n) >> 1
+
+            subscript += 1
+            U, V = U % n, V % n
+
+    return U, V
+
+
+def encode(plaintext, bits):
+	byte_array = bytearray(plaintext, 'utf-16')
+	encoded = []
+
+	chunk = bits // 8
+	j = -chunk
+
+	for i in range(len(byte_array)):
+		if i % chunk == 0:
+			j += chunk
+			encoded.append(0)
+
+		encoded[j // chunk] += byte_array[i] * (2 ** (8 * (i % chunk)))
+
+	return encoded
+
+
+def decode(encoded, bits):
+	byte_array = []
+	chunk = bits // 8
+
+	for integer in encoded:
+		for i in range(chunk):
+			copy = integer
+
+			for j in range(i + 1, chunk):
+				copy %= 2 ** (8 * j)
+
+			letter = copy // (2 ** (8 * i))
+			byte_array.append(letter)
+		
+			integer -= letter * 2 ** (8 * i)
+
+
+	decoded = bytearray(b for b in byte_array).decode('utf-16')
+	return decoded
---
+++ primality_tests.py
@@ -0,0 +1,146 @@
+from ntheory_utilities import *
+
+prime_cache = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 
+103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 
+239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 
+397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499]
+
+
+"""
+    @param: bits
+        number of bits in the prime to be generated
+
+    @returns:
+        probable prime in the given range [2 ^ (bits - 2), 2 ^ (bits - 1)]
+"""
+def generate_prime(bits):
+    while True:
+        p = random.randint(2 ** (bits - 2), 2 ** (bits - 1))
+        if baillie_psw(p) and baillie_psw(2 * p  + 1):
+            return 2 * p + 1
+
+
+"""
+    @param: n
+        candidate to be tested for primality, base 2
+
+    @returns:
+        True if the candidate is not base 2-liar
+        False if the number is composite
+"""
+def miller_rabin_base_2(n):
+    d = n - 1
+    s = 0
+
+    while not d & 1: 
+        d = d >> 1 
+        s += 1
+
+    x = pow(2, d, n)
+
+    if x == 1 or x == n - 1:
+        return True
+
+    for i in range(s - 1):
+        x = pow(x, 2, n) 
+
+        if x == 1:
+            return False
+
+        elif x == n - 1:
+            return True
+
+    return False
+
+
+"""
+    Lucas probable prime test (pseudoprime section) as illustrated on Wikipedia.
+    https://en.wikipedia.org/wiki/Lucas_pseudoprime#Lucas_probable_primes_and_pseudoprimes
+
+    @param: n
+        candidate to be tested for primality
+
+    @param P, Q:
+        Generators for the Lucas sequence 
+        where Uk(P, Q) and Vk(P, Q) be the corresponding Lucas sequences.
+
+    @param: D
+        D value such that Jacobi symbol (D / n) =- 1
+        details on the choosing of this value may be found
+        in the baillie - PSW function description
+        D = P ^ 2 - 4 * Q
+"""
+def lucas_pp(n, D, P, Q):                                                                                                                                                                                                                         
+
+    U, V = U_V_subscript(n + 1, n, 1, P, P, Q, D)
+
+    if U != 0:
+        return False
+
+    d = n + 1
+    s = 0
+
+    while not d & 1:
+        d = d >> 1
+        s += 1
+
+    U, V = U_V_subscript(n + 1, n, 1, P, P, Q, D)
+
+    if U == 0:
+        return True
+
+    for r in range(s):
+        U, V = (U * V) % n, (pow(V, 2, n) - 2 * pow(Q, d * (2 ** r), n)) % n
+        if V == 0:
+            return True
+
+    return False
+
+
+
+"""
+    Baillie - PSW probabilistic primality test, 
+    completely deterministic for candidates below 2^64.
+
+    Runs a sanity check through a small cache before running the Miller-Rabin
+    base 2 test and the Lucas pseudoprime test.
+
+    If the number is a perfect square, then there would be no point in trying
+    to find a D s.t. (D / n) = -1 since there is no solution, hence the candidate
+    is obviously a candidate.
+
+    Other implementation details, regarding the choosing of P and Q can be found
+    below.
+
+    https://en.wikipedia.org/wiki/Baillie%E2%80%93PSW_primality_test
+
+
+    @param: candidate
+        candidate to be tested for primality
+
+    @returns:
+        Boolean value indicating whether the candidate is
+        a probable prime or not
+"""
+def baillie_psw(candidate):
+    for prime in prime_cache:
+        if candidate == prime:
+            return True
+        elif candidate % prime == 0:
+            return False
+
+    if not miller_rabin_base_2(candidate):
+        return False
+    
+    if int(sqrt(candidate) + 0.5) ** 2 == candidate:
+        return False
+
+    D = 5
+    while jacobi_symbol(D, candidate) != -1:
+        D += 2 if D > 0 else -2
+        D *= -1
+    
+    if not lucas_pp(candidate, D, 1, (1 - D) / 4):
+        return False
+
+    return True
