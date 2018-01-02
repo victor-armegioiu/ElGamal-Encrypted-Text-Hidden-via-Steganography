7cab0dce93dfe4cadadc498d4ee8f0c814eb55fb
Victor Armegioiu <victor.armegioiu@gmail.com>
2018-01-02 23:08:10 +0200

    Update primality_tests.py

--- primality_tests.py
+++ primality_tests.py
@@ -1,3 +1,5 @@
+__author__ = "Victor Armegioiu"
+
 from ntheory_utilities import *
 
 prime_cache = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 
